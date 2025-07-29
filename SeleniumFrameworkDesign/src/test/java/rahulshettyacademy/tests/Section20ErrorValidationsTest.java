256c5058d29cb55a6401d62e91d21323093aab19
ReyndertLindeyer <rlindeyer@hotmail.com>
2025-07-29 16:04:38 -0400

    Finished section 22

--- SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/tests/Section20ErrorValidationsTest.java
+++ SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/tests/Section20ErrorValidationsTest.java
@@ -5,17 +5,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import rahulshettyacademy.TestComponents.Section20BaseTest;
+import rahulshettyacademy.TestComponents.Section22Retry;
 import rahulshettyacademy.pageobjects.Section19ProductCatalogue;
 
 public class Section20ErrorValidationsTest extends Section20BaseTest {
 
-	@Test(groups= {"ErrorHandling"})
+	@Test(groups= {"ErrorHandling"}, retryAnalyzer=Section22Retry.class)
 	public void LoginErrorValidation() {
 		//Input incorrect info
 		landingPage.LoginApplication("vASDbn@hmail.com", "NightC1ty2077");
 		
 		//Get error
-		Assert.assertEquals("Incorrect email or password.", landingPage.GetErrorMessage());
+		Assert.assertEquals("Incorrect mail or password.", landingPage.GetErrorMessage());
 	}
 	
 	@Test(groups= {"ErrorHandling"})
